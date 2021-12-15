ebdd247ca8bea39b728e2dc05692f1a6f55c0318
ravindaa <ravindaa@oregonstate.edu>
2021-12-15 18:28:03 +0600

    Tutorial Part 2

--- app/views.py
+++ app/views.py
@@ -1,14 +1,15 @@
 
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route('/')
 def index():
     return render_template('main.html')
 
-@app.route('/patients')
+@app.route('/patients', methods=['POST'])
 def patients():
-    return 'Display patients'
+    email_address = request.form['email-address']
+    return email_address
 
 @app.route('/patient')
 def individual_patient():
