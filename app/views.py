28250a585483a1c5a319f2060596652af95eb8c1
ravindaa <ravindaa@oregonstate.edu>
2022-01-15 18:32:30 +0600

    Full login logout commit

--- app/views.py
+++ app/views.py
@@ -2,11 +2,14 @@
 from werkzeug.utils import redirect
 from app import app, db
 from app.models import Doctor
-from flask import json, render_template, request, session, url_for, jsonify
+from flask import json, render_template, request, session, url_for, jsonify, flash
 
 @app.route('/')
 def index():
-    return render_template('main.html')
+    if 'doctor' in session.keys():
+        return render_template('patients.html')
+    else:
+        return render_template('main.html')
 
 @app.route('/register-doctor', methods=['POST'])
 def register_doctor():
@@ -19,7 +22,7 @@ def register_doctor():
     doctor.set_password(form['password'])
     db.session.add(doctor)
     db.session.commit()
-    return 'Successfully registered'
+    return redirect(url_for('index'))
 
 @app.route('/validate-doctor', methods=['POST'])
 def validate_doctor():
@@ -34,7 +37,7 @@ def validate_doctor():
 @app.route('/validate-password', methods=['POST'])
 def validate_password():
     if request.method == "POST":
-        email_address = request.get_json()['password']
+        email_address = request.get_json()['email']
         password = request.get_json()['password']
         userFound = 'false'
         passwordCorrect = 'false'
@@ -50,17 +53,27 @@ def validate_password():
 def login_doctor():
     form = request.form
     doctor = Doctor.query.filter_by(email=form['email-address']).first()
+    print(doctor.check_password(form['password']))
     if doctor.check_password(form['password']):
+        # if not, redirect to index and give flask message (don't worry about javascript validation)
         session['doctor'] = doctor.id
         return redirect(url_for('patients'))
+    else:
+        flash("Password was incorrect.")
+        return redirect(url_for('index'))
 
-
+@app.route('/logout-doctor', methods=['POST', 'GET'])
+def logout_doctor():
+    session.pop('doctor', None)
+    return redirect(url_for('index'))
 
 
 @app.route('/patients', methods=['POST', 'GET'])
 def patients():
-    doctor = session['doctor']
-    return 'Successfully logged in!'
+    doctor = None
+    if session['doctor']:
+        doctor = session['doctor']
+    return render_template('patients.html')
 
 @app.route('/patient')
 def individual_patient():
