d0d8fc09fdd04f475b83e0d5975fa99c2ccf8fc4
ravindaa <ravindaa@oregonstate.edu>
2022-02-18 17:49:42 +0600

    Recreated database and fixed dose addiont error

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 
 from werkzeug.utils import redirect
 from app import app, db
-from app.models import Doctor, Patient
+from app.models import Doctor, Patient, Vaccine_Dose
 from flask import json, render_template, request, session, url_for, jsonify, flash
 
 @app.route('/')
@@ -95,7 +95,45 @@ def register_patient():
         flash('Patient already exists')
         return redirect(url_for('patients'))
 
-@app.route('/patient')
-def individual_patient():
-    return 'Individual patient'
+@app.route('/vaccine-record/<patient_id>', methods=['GET', 'POST'])
+def vaccine_record(patient_id):
+    if not patient_id:
+        return redirect(url_for('patients'))
+    patient = Patient.query.filter_by(id=patient_id).first()
+    if patient:
+        vaccine_doses = Vaccine_Dose.query.filter_by(patient_id=patient_id)
+        return render_template('vaccine_record.html', vaccine_doses=vaccine_doses, patient_id=patient_id)
+    return render_template('vaccine_record.html')
+
+@app.route('/add-vaccine-record/<patient_id>', methods=['POST', 'GET'])
+def add_vaccine_record(patient_id):
+    form = request.form
+    dose_id = form['dose-id']
+    dose_number = form['dose-number']
+    vaccine_dose = Vaccine_Dose.query.filter_by(patient_id=patient_id, dose_no=dose_number).first()
+    if vaccine_dose:
+        flash('This dose number has already been entered.')
+        return redirect(url_for('vaccine_record', patient_id=patient_id))
+    vaccine_dose = Vaccine_Dose.query.filter_by(dose_id=dose_id).first()
+    if vaccine_dose:
+        flash('A dose with this serial number already exists in the database')
+        return redirect(url_for('vaccine_record', patient_id=patient_id))
+    vaccine_dose = Vaccine_Dose(
+        type=form['type'],
+        volume=form['volume'],
+        dose_no=form['dose-number'],
+        dose_id=dose_id,
+        patient_id=patient_id
+    )
+    db.session.add(vaccine_dose)
+    db.session.commit()
+    flash('Vaccine dose successfully added.')
+    return redirect(url_for('vaccine_record', patient_id=patient_id))
+    
+@app.route('/delete-vaccine-record/<patient_id>/<vaccine_id>', methods=['GET'])
+def delete_vaccine_record(patient_id, vaccine_id):
+    Vaccine_Dose.query.filter_by(id=vaccine_id).delete()
+    db.session.commit()
+    flash('Vaccine dose successfully deleted.')
+    return redirect(url_for('vaccine_record', patient_id=patient_id))
 
